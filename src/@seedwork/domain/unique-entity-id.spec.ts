41ede35f7c25f0b9433fdbd5e658cd786e75479a
Jefferson <jeffersonbraster@gmail.com>
2022-03-15 23:48:43 -0300

    refatorando testes

---
+++ src/@seedwork/domain/unique-entity-id.spec.ts
@@ -0,0 +1,33 @@
+import InvalidUuidError from "../../@seedwork/errors/invalid-uuid.error";
+import UniqueEntityId from "./unique-entity-id";
+import { validate as uuidValidade } from "uuid";
+
+describe("uniqueentityid tests", () => {
+  const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+
+  beforeEach(() => {
+    validateSpy.mockClear();
+  });
+
+  it("should throw error when uuid is invalid", () => {
+    expect(() => new UniqueEntityId("fake id!")).toThrow(
+      new InvalidUuidError()
+    );
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should accept uuid passed in constructor", () => {
+    const uuid = "ba2e710f-8ac8-477d-81d0-78237fce4b05";
+    const vo = new UniqueEntityId(uuid);
+
+    expect(vo.id).toBe(uuid);
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should validate uuid passed in constructor", () => {
+    const vo = new UniqueEntityId();
+
+    expect(uuidValidade(vo.id)).toBeTruthy();
+    expect(validateSpy).toHaveBeenCalled();
+  });
+});
